17245d533ec5f0a114b0348d1e9b1cbd7fd5027d
Guilherme Blanco <guilhermeblanco@gmail.com>
2017-05-05 23:54:39 -0400

    Removed isIdentifierComposite flag from ClassMetadata

--- lib/Doctrine/ORM/EntityManager.php
+++ lib/Doctrine/ORM/EntityManager.php
@@ -380,8 +380,8 @@ use Doctrine\Common\Util\ClassUtils;
     {
         $class = $this->metadataFactory->getMetadataFor(ltrim($entityName, '\\'));
 
-        if ( ! is_array($id)) {
-            if ($class->isIdentifierComposite) {
+        if (! is_array($id)) {
+            if ($class->isIdentifierComposite()) {
                 throw ORMInvalidArgumentException::invalidCompositeIdentifier();
             }
 
@@ -472,7 +472,7 @@ use Doctrine\Common\Util\ClassUtils;
         $class = $this->metadataFactory->getMetadataFor(ltrim($entityName, '\\'));
 
         if ( ! is_array($id)) {
-            if ($class->isIdentifierComposite) {
+            if ($class->isIdentifierComposite()) {
                 throw ORMInvalidArgumentException::invalidCompositeIdentifier();
             }
 
@@ -528,7 +528,7 @@ use Doctrine\Common\Util\ClassUtils;
         $class = $this->metadataFactory->getMetadataFor(ltrim($entityName, '\\'));
 
         if ( ! is_array($id)) {
-            if ($class->isIdentifierComposite) {
+            if ($class->isIdentifierComposite()) {
                 throw ORMInvalidArgumentException::invalidCompositeIdentifier();
             }
 
